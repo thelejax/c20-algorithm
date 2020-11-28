ff83c5795724405432d42f169e95919b151beea6
thelejax <73752553+thelejax@users.noreply.github.com>
2020-11-28 12:02:41 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,52 +1,56 @@
-var movingRect, fixedRect;
+var fixedRect, movingRect;
 
+var gameObject1,gameObject2,gameObject3,gameObject4;
 
 function setup() {
-  createCanvas(800,400);
-  fixedRect=createSprite(200,200,50,80);
-  movingRect=createSprite(200,300, 80,30);
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
 
-movingRect.shapeColor="green"
-fixedRect.shapeColor="green"
+gameObject1=createSprite(100,100,50,50);
+gameObject1.shapeColor= "green";
 
+gameObject2=createSprite(200,100,50,50);
+gameObject2.shapeColor= "green";
 
+gameObject3=createSprite(300,100,50,50);
+gameObject3.shapeColor= "green";
+
+gameObject4=createSprite(400,100,50,50);
+gameObject4.shapeColor= "green";
 
 }
 
 function draw() {
-  background(255,255,255);  
-movingRect.x=mouseX;
-movingRect.y=mouseY;
-
-if(movingRect.x-fixedRect.x < fixedRect.width/2+movingRect.width/2
-   && fixedRect.x-movingRect.x < fixedRect.width/2+movingRect.width/2
-   && fixedRect.y-movingRect.y < fixedRect.height/2+movingRect.height/2
-   && movingRect.y-fixedRect.y < fixedRect.height/2+movingRect.height/2
-   
-
-   ){
-   
-
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
   
 
+if(isTouching(movingRect, gameObject4)){
 
-  
+  movingRect.shapeColor = "pink";
+  gameObject4.shapeColor = "pink";
 
-
-fixedRect.shapeColor="red"
-movingRect.shapeColor="red"
 
 }
-  
+
+else{
+
+  movingRect.shapeColor = "green";
+  gameObject4.shapeColor = "green";
 
 
-
-
-else {
-  movingRect.shapeColor="green"
-  fixedRect.shapeColor="green"
 }
 
 
+
+
+  
   drawSprites();
 }
+
